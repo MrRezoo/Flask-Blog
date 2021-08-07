ab2b324d7433b785553d8efe243431088a3c3cbf
Mr.Rezoo <rezam578@gmail.com>
2021-08-07 13:53:24 +0430

    end of project

--- blog/__init__.py
+++ blog/__init__.py
@@ -1,8 +1,7 @@
 from flask import Flask
+from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
-from flask_bcrypt import Bcrypt
-
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///../blog.db'
@@ -13,4 +12,4 @@ login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'info'
 
-from blog import routes
+from blog import routes  # circular import error
